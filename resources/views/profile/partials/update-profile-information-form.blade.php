9c06198c7d421a171df6326bf5c64c10c5acb7d7
seanhw22 <119191741+seanhw22@users.noreply.github.com>
2024-06-25 17:47:16 +0700

    fixed ui on profile

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,5 +1,5 @@
 <section>
-    <header>
+    <header style="background: none;">
         <h2 class="text-lg font-medium text-gray-900">
             {{ __('Profile Information') }}
         </h2>
@@ -13,7 +13,7 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6 text-dark">
         @csrf
         @method('patch')
 
